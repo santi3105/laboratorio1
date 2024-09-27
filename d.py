847292ae137c1ef5a904569bef6ca8d8451bdb3b
Santiago Fandiño Nieto <fandinosantiago00@gmail.com>
2024-09-27 16:58:29 -0500

    se arreglo el porque al ejecutar el codigo mostraba none

--- d.py
+++ d.py
@@ -1,11 +1,11 @@
-usuario = int(input(print(f"Ingrese su nivel de usuario de 0-5: ")))
-tarjeta = input(print(f"Su tarjeta de identificacion esta activa? si o no: "))
-nivel = int(input(print(f"Ingrese a que nivel quiere ingresar ")))
+usuario = int(input(f"Ingrese su nivel de usuario de 0-5: "))
+tarjeta = input(f"Su tarjeta de identificacion esta activa? si o no: ")
+nivel = int(input(f"Ingrese a que nivel quiere ingresar "))
 if tarjeta == "si":
     tarjeta = True
 else:
     tarjeta = False
-contraseña = int(input(print(f"Hace cuantos dias cambio su contraseña: ")))
+contraseña = int(input(f"Hace cuantos dias cambio su contraseña: "))
 
 if (nivel ==5 and usuario == 5 and contraseña <= 30 and tarjeta == True):
     print("acceso a nivel 5 concedido")
